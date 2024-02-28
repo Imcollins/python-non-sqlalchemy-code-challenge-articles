37f38f60431da0c6683233c927032b292793fbc4
Rene <racosta323@gmail.com>
2024-02-28 09:10:06 -0600

    inits author class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -8,6 +8,23 @@ class Author:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name (self, new_name):
+        if hasattr(self, "name"):
+            raise AttributeError("Name cannot be changed")
+        else:
+            if isinstance(new_name, str):
+                if len(new_name):
+                    self._name = new_name
+                else:
+                    raise ValueError("Name must be longer than 0 characters")
+            else:
+                raise TypeError("Name must be a string")
+
     def articles(self):
         pass
 
--- lib/debug.py
+++ lib/debug.py
@@ -7,7 +7,9 @@ from classes.many_to_many import Magazine
 
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
-
+    author_1 = Author("Carry Bradshaw")
+    author_2 = Author("Nathaniel Hawthorne")
+    print (author_1.name)
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
